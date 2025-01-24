4d2a2e98754af8a02aa3e53abc1235fd8933e049
mayantra <isaac.vassquez@gmail.com>
2025-01-24 16:52:28 -0600

    todo el commit

---
+++ resources/index.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.menu', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+    sr.reveal('.mensajePrincipal', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+    sr.reveal('.contImg', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+    sr.reveal('.encabezado', {
+        duration: 500,
+        opacity: 0.6,
+    });
+    sr.reveal('.escalas', {
+        scale: 0.5,
+    });
